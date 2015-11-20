2f38be00043ad61cb5da564e5d5103d0aef1cf3c
Matt Parker <mpdairy@gmail.com>
2015-11-20 00:08:41 -0500

    cleaned up

--- examples/drinkers/resources/public/js/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [posh.example] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
--- examples/drinkers/resources/public/js/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map
