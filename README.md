0bb67658c7b2b5ddc1a9aaf070849aff657baa6d
Matt Parker <mpdairy@gmail.com>
2016-11-29 20:09:33 -0700

    0.5.5

--- README.md
+++ README.md
@@ -60,7 +60,7 @@ buttons ([trashy live demo](http://otherway.org/posh-todo/)).
 Start a Reagent project and include these dependencies:
 
 ```clj
-[posh "0.5.4"]
+[posh "0.5.5"]
 ```
 
 Require in Reagent app files:
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject posh "0.5.4"
+(defproject posh "0.5.5"
   :description "Luxuriously easy and powerful Reagant / Datascript front-end framework"
   :url "http://github.com/mpdairy/posh/"
   :license {:name "Eclipse Public License"
