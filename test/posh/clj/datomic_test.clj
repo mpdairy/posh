2a7c1ad3d7e65c7fa018b800f0437a0844b90467
Alex Gunnarson <alexandergunnarson@gmail.com>
2017-01-17 12:24:46 -0500

    Test `remove-watch`

--- test/posh/clj/datomic_test.clj
+++ test/posh/clj/datomic_test.clj
@@ -69,23 +69,33 @@
             notified (atom 0)
             _ (r/add-eager-watch sub :k (fn [_ _ _ _] (swap! notified inc)))
             notified-no-deref (atom 0)
-            _ (r/add-eager-watch sub-no-deref :k-no-deref (fn [_ _ _ _] (swap! notified-no-deref inc)))
-            txn-report (db/transact! conn
-                         [{:db/id     (tempid)
-                           :test/attr "Abcde"}])
-            _ (do @sub @sub @sub @sub)
-            _ (is (= @sub
-                     @(db/q [:find '?e
-                             :where ['?e :test/attr]]
-                            conn)
-                     (d/q [:find '?e
-                           :where ['?e :test/attr]]
-                           (db/db* conn))))
-            _ (is (= @notified 1))
-            _ (is (= @notified-no-deref 1))
-            txn-report (db/transact! conn
-                         [{:db/id     (tempid)
-                           :test/attr "Fghijk"}])
-            _ (do @sub @sub @sub @sub @sub)
-            _ (is (= @notified 2))
-            _ (is (= @notified-no-deref 2))]))))
+            _ (r/add-eager-watch sub-no-deref :k-no-deref (fn [_ _ _ _] (swap! notified-no-deref inc)))]
+      (testing "Listeners are notified correctly"
+        (db/transact! conn
+          [{:db/id     (tempid)
+            :test/attr "Abcde"}])
+        (do @sub @sub @sub @sub)
+        (is (= @sub
+               @(db/q [:find '?e
+                       :where ['?e :test/attr]]
+                      conn)
+               (d/q [:find '?e
+                     :where ['?e :test/attr]]
+                     (db/db* conn))))
+        (is (= @notified 1))
+        (is (= @notified-no-deref 1))
+        (db/transact! conn
+          [{:db/id     (tempid)
+            :test/attr "Fghijk"}])
+        (do @sub @sub @sub @sub @sub)
+        (is (= @notified 2))
+        (is (= @notified-no-deref 2)))
+      (testing "Remove-watch happens correctly"
+        (remove-watch sub :k)
+        (remove-watch sub :k-no-deref)
+        (db/transact! conn
+          [{:db/id     (tempid)
+            :test/attr "Lmnop"}])
+        (do @sub @sub @sub @sub @sub @sub)
+        (is (= @notified 2))
+        (is (= @notified-no-deref 2)))))))
