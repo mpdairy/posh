a92f7c77e15183f426cb877cd5c6dd4da45bd538
Alex Gunnarson <alexandergunnarson@gmail.com>
2017-01-17 20:05:32 -0500

    Move most of clj/datomic to lib/datomic

--- src/posh/clj/datomic.clj
+++ src/posh/clj/datomic.clj
@@ -1,138 +1,21 @@
 (ns posh.clj.datomic
   "The public API of Posh's Datomic implementation (for Clojure)."
-  (:require [posh.plugin-base :as base]
-            [posh.lib.ratom :as rx]
-            [clojure.core.async :as async
-              :refer [thread offer! <!! promise-chan]]
-            [datomic.api :as d]
-            [posh.lib.util :as u
-              :refer [debug]]))
-
-(defn datom->seq [db-after ^datomic.Datom d]
-  [(.e d) (d/ident db-after (.a d)) (.v d) (.tx d) (.added d)])
-
-; TODO import stuartsierra.component ?
-(defprotocol Lifecycle
-  (start [this])
-  (stop  [this]))
-
-(defn normalized-tx-report [{:keys [db-after] :as tx-report}]
-  (update tx-report :tx-data
-    (fn [datoms] (mapv #(datom->seq db-after %) datoms))))
-
-(defn run-listeners! [pconn tx-report']
-  (try (doseq [[_ callback] @(:listeners pconn)] (callback tx-report'))
-       (catch Throwable e (debug "WARNING:" e))))
-
-(defrecord PoshableConnection [datomic-conn listeners deduplicate-tx-idents interrupted?]
-  Lifecycle
-  (start [this]
-    (assert (instance? datomic.Connection datomic-conn))
-    (assert (instance? clojure.lang.IAtom listeners))
-    (assert (instance? clojure.lang.IAtom interrupted?))
-    ; See `transact!*` as to why the below schema+entity is required.
-    @(d/transact datomic-conn
-       [{:db/id                 (d/tempid :db.part/db)
-         :db.install/_attribute :db.part/db
-         :db/ident              :posh.clj.datomic.tx-notifier/value
-         :db/cardinality        :db.cardinality/one
-         :db/valueType          :db.type/uuid}
-        {:db/id    (d/tempid :db.part/db)
-         :db/ident ::tx-notifier}])
-    (thread
-      (loop []
-        (when-not @interrupted?
-                     ; `poll` because if `take`, still won't be nil or stop waiting when conn is released
-                     ; the poll time is how long it will take to shut down when that time comes
-          (when-let [{:keys [db-after] :as tx-report}
-                       (.poll ^java.util.concurrent.BlockingQueue
-                              (d/tx-report-queue datomic-conn)
-                              1
-                              java.util.concurrent.TimeUnit/SECONDS)]
-            (try (let [{:keys [tx-data] :as tx-report'} (normalized-tx-report tx-report)
-                       last-tx-item (last tx-data)
-                       tx-ident (when (and last-tx-item
-                                           (= (d/ident db-after (get last-tx-item 0)) ::tx-notifier)
-                                           (= (d/ident db-after (get last-tx-item 1)) :posh.clj.datomic.tx-notifier/value))
-                                  (get last-tx-item 2))]
-                   (try (debug "tx-report received in PoshableConnection")
-                        (when-not (get @deduplicate-tx-idents tx-ident)
-                          (run-listeners! this tx-report'))
-                     (finally
-                       (swap! deduplicate-tx-idents #(disj % tx-ident)))))
-              (catch Throwable e (debug "WARNING:" e)))
-            (recur)))))
-    this)
-  (stop [this]
-    (reset! interrupted? true)
-    (swap! deduplicate-tx-idents empty)
-    this))
-
-(defn ->poshable-conn [datomic-conn]
-  {:pre [(instance? datomic.Connection datomic-conn)]}
-  (let [listeners (atom nil)]
-    (with-meta (start (PoshableConnection. datomic-conn listeners (atom #{}) (atom false)))
-               {:listeners listeners})))
-
-(defn conn? [x] (instance? PoshableConnection x))
-
-(defn ->conn [x]
-  (if (conn? x)
-      (:datomic-conn x)
-      x))
-
-(defn assert-pconn [x] (assert (instance? PoshableConnection x)))
-
-(defn listen!
-  ([conn callback] (listen! conn (gensym) callback))
-  ([conn key callback]
-     {:pre [(conn? conn)]}
-     (swap! (:listeners (meta conn)) assoc key callback)
-     key))
-
-(defn db* [x]
-  (cond (instance? datomic.Database x)
-        x
-        (conn? x)
-        (-> x :datomic-conn d/db)
-        (instance? datomic.Connection x)
-        (d/db x)
-        :else x #_(throw (ex-info "Object cannot be converted into DB" {:obj x}))))
-
-(defn q* [q x & args]
-  (apply d/q q (db* x) args))
-
-(defn transact!*
-  "The main point of the additions onto Datomic's base `transact` fn is to wait for related
-   listeners to be run before returning."
-  [conn tx]
-  {:pre [(conn? conn)]}
-  (let [; In order to ensure listeners are run only once (i.e. deduplicate them),
-        ; we have to transmit to the report queue in a race-condition-free way
-        ; some sort of unique ID we know ahead of time. I'd like to just use the
-        ; txn ID, but this is not given ahead of time. Thus we must pass a squuid
-        ; to the transaction.
-        ; This is cleaner than e.g. using channels because they introduce race
-        ; conditions in this situation.
-        tx-ident   (d/squuid)
-        _          (swap! (:deduplicate-tx-idents conn) conj tx-ident)
-        tx-report  @(d/transact (->conn conn)
-                      (conj (vec tx) [:db/add ::tx-notifier :posh.clj.datomic.tx-notifier/value tx-ident]))
-        tx-report' (normalized-tx-report tx-report)
-        _          (run-listeners! conn tx-report')]
-    tx-report'))
+  (:require [posh.plugin-base   :as base]
+            [posh.lib.ratom     :as rx]
+            [datomic.api        :as d]
+            [posh.lib.datomic   :as l]))
 
 (def dcfg
-  (let [dcfg {:db            db*
+  (let [dcfg {:db            l/db*
               :pull*         d/pull
-              :q             q*
+              :q             l/q*
               :filter        d/filter
               :with          d/with
               :entid         d/entid
-              :transact!     transact!*
-              :listen!       listen!
-              :conn?         conn?
-              :->poshable-conn ->poshable-conn
+              :transact!     l/transact!*
+              :listen!       l/listen!
+              :conn?         l/conn?
+              :->poshable-conn l/->poshable-conn
               :ratom         rx/atom
               :make-reaction rx/make-reaction}]
    (assoc dcfg :pull (partial base/safe-pull dcfg))))
--- src/posh/lib/datomic.clj
+++ src/posh/lib/datomic.clj
@@ -1,12 +1,101 @@
 (ns posh.lib.datomic
   "General Datomic utils."
   (:require [datomic.api      :as d]
-            [posh.clj.datomic :as db])
-  (:import posh.clj.datomic.PoshableConnection))
+            [clojure.core.async :as async
+              :refer [thread offer! <!! promise-chan]]
+            [posh.plugin-base :as base]
+            [posh.lib.util    :as u
+              :refer [debug]]))
+
+; TODO import stuartsierra.component ?
+; TODO move
+(defprotocol Lifecycle
+  (start [this])
+  (stop  [this]))
+
+#_(defn datom->seq [^datomic.Datom d] [(.e d) (.a d) (.v d) (.tx d) (.added d)])
+
+(defn datom->seq [db-after ^datomic.Datom d]
+  [(.e d) (d/ident db-after (.a d)) (.v d) (.tx d) (.added d)])
+
+(extend-type datomic.Datom
+  clojure.core.protocols/CollReduce
+  (coll-reduce [datom f  ] (reduce f (f) (datom->seq datom)))
+  (coll-reduce [datom f v] (reduce f v   (datom->seq datom))))
+
+(defn normalized-tx-report [{:keys [db-after] :as tx-report}]
+  (update tx-report :tx-data
+    (fn [datoms] (mapv #(datom->seq db-after %) datoms))))
+
+(defn run-listeners! [pconn tx-report']
+  (try (doseq [[_ callback] @(:listeners pconn)] (callback tx-report'))
+       (catch Throwable e (debug "WARNING:" e))))
+
+(defrecord PoshableConnection [datomic-conn listeners deduplicate-tx-idents interrupted?]
+  Lifecycle
+  (start [this]
+    (assert (instance? datomic.Connection datomic-conn))
+    (assert (instance? clojure.lang.IAtom listeners))
+    (assert (instance? clojure.lang.IAtom interrupted?))
+    ; See `transact!*` as to why the below schema+entity is required.
+    ; TODO use `posh.lib.datomic/transact-schemas!`?
+    @(d/transact datomic-conn
+       [{:db/id                 (d/tempid :db.part/db)
+         :db.install/_attribute :db.part/db
+         :db/ident              :posh.clj.datomic.tx-notifier/value
+         :db/cardinality        :db.cardinality/one
+         :db/valueType          :db.type/uuid}
+        {:db/id    (d/tempid :db.part/db)
+         :db/ident ::tx-notifier}])
+    (thread
+      (loop []
+        (when-not @interrupted?
+                     ; `poll` because if `take`, still won't be nil or stop waiting when conn is released
+                     ; the poll time is how long it will take to shut down when that time comes
+          (when-let [{:keys [db-after] :as tx-report}
+                       (.poll ^java.util.concurrent.BlockingQueue
+                              (d/tx-report-queue datomic-conn)
+                              1
+                              java.util.concurrent.TimeUnit/SECONDS)]
+            (try (let [{:keys [tx-data] :as tx-report'} (normalized-tx-report tx-report)
+                       last-tx-item (last tx-data)
+                       tx-ident (when (and last-tx-item
+                                           (= (d/ident db-after (get last-tx-item 0)) ::tx-notifier)
+                                           (= (d/ident db-after (get last-tx-item 1)) :posh.clj.datomic.tx-notifier/value))
+                                  (get last-tx-item 2))]
+                   (try (debug "tx-report received in PoshableConnection")
+                        (when-not (get @deduplicate-tx-idents tx-ident)
+                          (run-listeners! this tx-report'))
+                     (finally
+                       (swap! deduplicate-tx-idents #(disj % tx-ident)))))
+              (catch Throwable e (debug "WARNING:" e)))
+            (recur)))))
+    this)
+  (stop [this]
+    (reset! interrupted? true)
+    (swap! deduplicate-tx-idents empty)
+    this))
+
+(defn conn? [x] (instance? PoshableConnection x))
+
+(defn ->poshable-conn [datomic-conn]
+  {:pre [(instance? datomic.Connection datomic-conn)]}
+  (let [listeners (atom nil)]
+    (with-meta (start (PoshableConnection. datomic-conn listeners (atom #{}) (atom false)))
+               {:listeners listeners})))
+
+(defn listen!
+  ([conn callback] (listen! conn (gensym) callback))
+  ([conn key callback]
+    {:pre [(conn? conn)]}
+    (swap! (:listeners (meta conn)) assoc key callback)
+    key))
 
 (def default-partition :db.part/default)
 
 (defn tempid [] (d/tempid default-partition))
+
+(defn ->conn [x] (if (conn? x) (:datomic-conn x) x))
 
 (defn install-partition [part]
   (let [id (d/tempid :db.part/db)]
@@ -15,19 +104,52 @@
      [:db/add               :db.part/db
       :db.install/partition id]]))
 
+(defn transact!*
+  "The main point of the additions onto Datomic's base `transact` fn is to wait for related
+   listeners to be run before returning."
+  [conn tx]
+  {:pre [(conn? conn)]}
+  (let [; In order to ensure listeners are run only once (i.e. deduplicate them),
+        ; we have to transmit to the report queue in a race-condition-free way
+        ; some sort of unique ID we know ahead of time. I'd like to just use the
+        ; txn ID, but this is not given ahead of time. Thus we must pass a squuid
+        ; to the transaction.
+        ; This is cleaner than e.g. using channels because they introduce race
+        ; conditions in this situation.
+        tx-ident   (d/squuid)
+        _          (swap! (:deduplicate-tx-idents conn) conj tx-ident)
+        tx-report  @(d/transact (->conn conn)
+                      (conj (vec tx) [:db/add ::tx-notifier :posh.clj.datomic.tx-notifier/value tx-ident]))
+        tx-report' (normalized-tx-report tx-report)
+        _          (run-listeners! conn tx-report')]
+    tx-report'))
+
 (defn transact-schemas!
   "This is used because, perhaps very strangely, schema changes to Datomic happen
    asynchronously."
   {:todo #{"Make more robust"}}
-  [conn schemas]
-  (let [txn-report (db/transact! conn
+  [dcfg conn schemas]
+  (let [txn-report (base/transact! dcfg conn
                      (->> schemas
-                          (map #(assoc % :db/id (d/tempid :db.part/db)
-                                         :db.install/_attribute :db.part/db))))
+                          (map (fn [[ident v]] (assoc v :db/ident ident
+                                                        :db/id    (d/tempid :db.part/db)
+                                                        :db.install/_attribute :db.part/db)))))
         txn-id     (-> txn-report :tx-data first (get 3))
-        _ #_(deref (d/sync (db/->conn conn) (java.util.Date. (System/currentTimeMillis))) 500 nil)
-            (deref (d/sync-schema (db/->conn conn) (inc txn-id)) 500 nil)] ; frustratingly, doesn't even work with un-`inc`ed txn-id
+        _ #_(deref (d/sync (->conn conn) (java.util.Date. (System/currentTimeMillis))) 500 nil)
+            (deref (d/sync-schema (->conn conn) (inc txn-id)) 500 nil)] ; frustratingly, doesn't even work with un-`inc`ed txn-id
     txn-report))
+
+(defn db* [x]
+  (cond (instance? datomic.Database x)
+        x
+        (conn? x)
+        (-> x :datomic-conn d/db)
+        (instance? datomic.Connection x)
+        (d/db x)
+        :else x #_(throw (ex-info "Object cannot be converted into DB" {:obj x}))))
+
+(defn q* [q x & args]
+  (apply d/q q (db* x) args))
 
 (defn with-conn [uri f]
   (try (d/create-database uri)
@@ -36,13 +158,13 @@
            (finally (d/release conn))))
     (finally (d/delete-database uri))))
 
-(defn with-posh-conn [retrieve uri schemas f]
+(defn with-posh-conn [dcfg retrieve uri schemas f]
   (with-conn uri
     (fn [conn*]
-      (let [poshed (db/posh-one! conn* retrieve) ; This performs a `with-meta` so the result is needed
-            conn   (-> poshed :conns :conn0) ; Has the necessary meta ; TODO simplify this
+      (let [poshed (base/posh-one! dcfg conn* retrieve) ; This performs a `with-meta` so the result is needed
+            conn   (-> poshed deref :conns :conn0) ; Has the necessary meta ; TODO simplify this
             _      (assert (instance? PoshableConnection conn))]
-        (try (let [txn-report (db/transact! conn (install-partition default-partition))
-                   txn-report (transact-schemas! conn schemas)]
-               (f conn))
-          (finally (db/stop conn))))))) ; TODO `unposh!`
+        (try (let [txn-report (base/transact! dcfg conn (install-partition default-partition))
+                   txn-report (transact-schemas! dcfg conn schemas)]
+               (f poshed conn))
+          (finally (stop conn))))))) ; TODO `unposh!`
